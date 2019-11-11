b91a61324c5b4c251760ac1eab409196bfb32812
Ivo <irbatistela@gmail.com>
2019-11-11 02:46:25 -0300

    Test build with php project in a folder

---
+++ test-folder/test/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase {
+
+	public function test_shouldJustTest(): void {
+		Assert::assertTrue(true);
+	}
+
+}
