74290795e949cc9dc87b9261d89caf15046be782
Ivo <irbatistela@gmail.com>
2020-01-26 04:15:34 -0300

    Add script to push all images at once

--- push-all-images-to-registry.sh
+++ push-all-images-to-registry.sh
@@ -6,7 +6,7 @@ find test/ -maxdepth 1 -mindepth 1 -type d | while read DIR_WITH_EXERCISE_TESTS;
 
   EXERCISE_ID=`echo "$DIR_WITH_EXERCISE_TESTS" | sed -e "s/$TEST_DIT_PATH_PATTERN//g"`
 
-  echo "Building $EXERCISE_ID image..."
+  echo "Pushing $EXERCISE_ID image..."
 
-  ./build-non-versioned-image-for-exercise.sh "$EXERCISE_ID"
+  docker push "byivo/mdc-php-checker:$EXERCISE_ID"
 done
