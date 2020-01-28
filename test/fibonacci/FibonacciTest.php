2460b023f322e1ae460c83b9fd00982fc006b914
Rhuan Gabriel <rhuan.santos@db1.com.br>
2020-01-27 21:07:51 -0300

    Updated Fibonacci Sequence tests

--- test/fibonacci/FibonacciTest.php
+++ test/fibonacci/FibonacciTest.php
@@ -2,47 +2,78 @@
 
 namespace Test;
 
+use DateTime;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
 class FibonacciTest extends TestCase
 {
-    public function getFirstTenNumbersOfTheFibonacciSequence(): array
+    public function getNumbersOneOfTenWithExpectedFibonacciNumbers(): array
     {
         return [
-            [
-                ['1', '1', '2', '3', '5', '8', '13', '21', '34', '55']
-            ]
+            ['1', '1',],
+            ['2', '1',],
+            ['3', '2',],
+            ['4', '3',],
+            ['5', '5',],
+            ['6', '8',],
+            ['7', '13',],
+            ['8', '21',],
+            ['9', '34',],
+            ['10', '55']
         ];
     }
 
     /**
      * @test
-     * @dataProvider getFirstTenNumbersOfTheFibonacciSequence
-     * @param array $FibonacciSequence
+     * @dataProvider getNumbersOneOfTenWithExpectedFibonacciNumbers
+     * @param string $sequenceNumber
+     * @param string $fibonacciNumber
      */
-    public function givenFirstTenNumbersOfTheFibonacciSequence_ShouldAssertReturnedNumbers(array $FibonacciSequence): void
+    public function givenFirstTenNumbersOfTheFibonacciSequence_ShouldAssertReturnedNumbers(
+        string $sequenceNumber,
+        string $fibonacciNumber
+    ): void
     {
-        $number = 1;
-        $returnedValues = [];
-        $errorMessage = 'Error calculating the first ten numbers of the Fibonacci sequence';
+        $returnValue = $this->runExerciseWithParameter($sequenceNumber);
+        $errorMessage = "Error to assert $returnValue with $fibonacciNumber";
+        Assert::assertEquals($fibonacciNumber, $returnValue, $errorMessage);
+    }
 
-        while ($number <= 10) {
-            $returnedValues[] = $this->runExerciseWithParameter($number);
-            $number++;
-        }
-
-        Assert::assertEquals($FibonacciSequence, $returnedValues, $errorMessage);
+    public function getLargeNumbersWithExpectedFibonacciNumbers(): array
+    {
+        return [
+            ['30', '832040',],
+            ['32', '2178309',],
+            ['35', '9227465',],
+            ['37', '24157817',]
+        ];
     }
 
     /**
      * @test
+     * @dataProvider getLargeNumbersWithExpectedFibonacciNumbers
+     * @param string $inputNumber
+     * @param string $fibonacciNumber
      */
-    public function givenLargeNumber_ShouldCalculateTheFibonacciNumber(): void
+    public function givenLargeNumber_ShouldCalculateTheFibonacciNumber(
+        string $inputNumber,
+        string $fibonacciNumber
+    ): void
     {
-        $errorMessage = 'Error when calculating a large number of the Fibonacci sequence';
-        $returnValue = $this->runExerciseWithParameter('521');
-        Assert::assertEquals('3.4125228500685E+108', $returnValue, $errorMessage);
+        $errorMessageAssertNumbers = 'Error when calculating a large number of the Fibonacci sequence';
+        $errorMessageTimeOut = 'Error time out';
+
+        $testStartDate = new DateTime('now');
+        $secondInitialTest = $testStartDate->format('ms');
+
+        $returnValue = $this->runExerciseWithParameter($inputNumber);
+
+        $testEndTime = new DateTime('now');
+        $secondEndTest = $testEndTime->format('ms');
+
+        Assert::assertEquals($fibonacciNumber, $returnValue, $errorMessageAssertNumbers);
+        Assert::assertEqualsWithDelta($secondInitialTest, $secondEndTest, 1, $errorMessageTimeOut);
     }
 
     private function runExerciseWithParameter(string $parameter): string
