3fb0d7ba08893850625a41ae084c2ff7e4c52f3b
Rhuan Santos <rhuan.santos@db1.com.br>
2020-01-31 19:43:50 +0000

    Fix indentation

--- test/palindrome/PalindromeTest.php
+++ test/palindrome/PalindromeTest.php
@@ -7,46 +7,47 @@ use PHPUnit\Framework\TestCase;
 
 class PalindromeTest extends TestCase {
 
-	public function getPalindromesPhrasesWithExpectedResult(): array {
-		return [
+    public function getPalindromesPhrasesWithExpectedResult(): array {
+        return [
             ['a nut for a jar of tuna', 'a nut for a jar of tuna'],
             ['borrow or rob', 'borrow or rob'],
             ['yo banana boy', 'yo banana boy'],
-		];
+        ];
 	}
 
-	/**
-	 * @test
-	 * @dataProvider getPalindromesPhrasesWithExpectedResult
-	 */
-	public function givenPalindromePhrases_ShouldPrintPhrases(
+    /**
+     * @test
+     * @dataProvider getPalindromesPhrasesWithExpectedResult
+     */
+    public function givenPalindromePhrases_ShouldPrintPhrases(
         string $phrases,
         string $expected
     ): void {
         $returnValue = $this->runExerciseWithParameter($phrases);
-		Assert::assertEquals($expected, $returnValue);
-	}
+        Assert::assertEquals($expected, $returnValue);
+    }
 
-	public function getPhrasesNotPalindromesWithExpectedResult(): array {
-		return [
+    public function getPhrasesNotPalindromesWithExpectedResult(): array {
+        return [
             ['hello world', ''],
             ['the best friend', ''],
             ['code master', ''],
-		];
-	}
+        ];
+    }
 
-	/**
-	 * @test
-	 * @dataProvider getPhrasesNotPalindromesWithExpectedResult
-	 */
-	public function givenPhrasesNotPalindromes_ShouldPrintNothing(
+    /**
+     * @test
+     * @dataProvider getPhrasesNotPalindromesWithExpectedResult
+     */
+    public function givenPhrasesNotPalindromes_ShouldPrintNothing(
         string $phrases,
         string $expected
     ): void {
         $returnValue = $this->runExerciseWithParameter($phrases);
-		Assert::assertEquals($expected, $returnValue);
-	}
-	private function runExerciseWithParameter(string $parameter): string {
-	    return exec('php -f /opt/project/public/index.php "' . $parameter . '"');
-	}
+        Assert::assertEquals($expected, $returnValue);
+    }
+    
+    private function runExerciseWithParameter(string $parameter): string {
+        return exec('php -f /opt/project/public/index.php "' . $parameter . '"');
+    }
 }
